d72ab54dfbcea4b6e0d121d2d18ceaf784d3cc15
thanhdung26031994 <thanhdung26031994@gmail.com>
2024-03-06 08:17:11 +0700

    validate infor user

--- src/main/java/com/example/validateinforuser/model/User.java
+++ src/main/java/com/example/validateinforuser/model/User.java
@@ -1,2 +1,38 @@
-package com.example.validateinforuser.model;public class User {
+package com.example.validateinforuser.model;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+public class User {
+    @NotEmpty
+    @Size(min = 2, max = 30)
+    private String name;
+
+    @Min(18)
+    private Integer age;
+
+    public User() {
+    }
+
+    public User(@NotEmpty @Size(min = 2, max = 30) String name,@Min(18) Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
